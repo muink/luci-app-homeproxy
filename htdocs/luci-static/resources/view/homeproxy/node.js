immortalwrt/homeproxy

feat(generator/node): Add `gso` support for wireguard node

htdocs/luci-static/resources/view/homeproxy/node.js
@@ -944,6 +944,12 @@ return view.extend({
 		/* Transport config end */
 
 		/* Wireguard config start */
+		so = ss.option(form.Flag, 'wireguard_gso', _('Generic segmentation offload'));
+		so.default = so.disabled;
+		so.depends('type', 'wireguard');
+		so.rmempty = false;
+		so.modalonly = true;
+
 		so = ss.option(form.DynamicList, 'wireguard_local_address', _('Local address'),
 			_('List of IP (v4 or v6) addresses prefixes to be assigned to the interface.'));
 		so.datatype = 'cidr';

muink/luci-app-homeproxy

htdocs/luci-static/resources/view/homeproxy/node.js
@@ -1045,6 +1045,12 @@ return view.extend({
 		/* Transport config end */
 
 		/* Wireguard config start */
+		o = s.option(form.Flag, 'wireguard_gso', _('Generic segmentation offload'));
+		o.default = o.disabled;
+		o.depends('type', 'wireguard');
+		o.rmempty = false;
+		o.modalonly = true;
+
 		o = s.option(form.DynamicList, 'wireguard_local_address', _('Local address'),
 			_('List of IP (v4 or v6) addresses prefixes to be assigned to the interface.'));
 		o.datatype = 'cidr';