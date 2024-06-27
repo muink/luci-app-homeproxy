muink/luci-app-homeproxy

commit 14c46a100a2796e13ddbb1d916d51022889b46ca

feat(generator/node): Add nodes filter support to `selector` node

htdocs/luci-static/resources/view/homeproxy/node.js
@@ -875,6 +875,20 @@ return view.extend({
 		o.default = '';
 		o.depends({'group': /^$/, 'type': 'selector'});
 		o.modalonly = true;
+
+		o = s.option(form.ListValue, 'filter_nodes', _('Filter nodes'),
+			_('Drop/keep specific nodes from outbounds.'));
+		o.value('', _('Disable'));
+		o.value('blacklist', _('Blacklist mode'));
+		o.value('whitelist', _('Whitelist mode'));
+		o.default = '';
+		o.depends('type', 'selector');
+		o.modalonly = true;
+
+		o = s.option(form.DynamicList, 'filter_keywords', _('Filter keywords'),
+			_('Drop/keep nodes that contain the specific keywords. <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions">Regex</a> is supported.'));
+		o.depends({'filter_nodes': '', '!reverse': true});
+		o.modalonly = true;
 		/* Selector config end */
 
 		/* URLTest config start */

root/etc/homeproxy/scripts/update_subscriptions.uc
@@ -16,7 +16,7 @@ import { init_action } from 'luci.sys';
 
 import {
 	calcStringMD5, wGET, executeCommand, decodeBase64Str,
-	getTime, isEmpty, parseURL, validation,
+	getTime, isEmpty, parseURL, validation, filterCheck,
 	HP_DIR, RUN_DIR
 } from 'homeproxy';
 
@@ -46,21 +46,6 @@ if (routing_mode !== 'custom') {
 /* UCI config end */
 
 /* String helper start */
-function filter_check(name) {
-	if (isEmpty(name) || filter_mode === 'disabled' || isEmpty(filter_keywords))
-		return false;
-
-	let ret = false;
-	for (let i in filter_keywords) {
-		const patten = regexp(i);
-		if (match(name, patten))
-			ret = true;
-	}
-	if (filter_mode === 'whitelist')
-		ret = !ret;
-
-	return ret;
-}
 /* String helper end */
 
 /* Common var start */
@@ -495,7 +480,7 @@ function main() {
 			      nameHash = calcStringMD5(label);
 			config.label = label;
 
-			if (filter_check(config.label))
+			if (filterCheck(config.label, filter_mode, filter_keywords))
 				log(sprintf('Skipping blacklist node: %s.', config.label));
 			else if (node_cache[groupHash][confHash] || node_cache[groupHash][nameHash])
 				log(sprintf('Skipping duplicate node: %s.', config.label));

root/etc/homeproxy/scripts/generate_client.uc
@@ -13,7 +13,7 @@ import { cursor } from 'uci';
 
 import {
 	executeCommand, shellQuote, calcStringCRC8, calcStringMD5, isEmpty, strToBool, strToInt,
-	removeBlankAttrs, validateHostname, validation,
+	removeBlankAttrs, validateHostname, validation, filterCheck,
 	HP_DIR, RUN_DIR
 } from 'homeproxy';
 
@@ -159,7 +159,7 @@ function parse_dnsquery(strquery) {
 
 }
 
-function get_tag(cfg) {
+function get_tag(cfg, failback_tag, filterable) {
 	if (isEmpty(cfg))
 		return null;
 
@@ -173,8 +173,13 @@ function get_tag(cfg) {
 			node = uci.get_all(uciconfig, cfg);
 	}
 
+	//filter check
+	if (!isEmpty(filterable))
+		if (filterCheck(node.label, filterable.filter_nodes, filterable.filter_keywords))
+			return null;
+
 	const sub_info = subs_info[node.grouphash];
-	return node.label ? sprintf("%s%s", node.grouphash ? sprintf("[%s] ", sub_info ? (sub_info.name ? sub_info.name : 'Group' + sub_info.order) : calcStringCRC8(node.grouphash)) : '', node.label) : null;
+	return node.label ? sprintf("%s%s", node.grouphash ? sprintf("[%s] ", sub_info ? (sub_info.name ? sub_info.name : 'Group' + sub_info.order) : calcStringCRC8(node.grouphash)) : '', node.label) : (failback_tag || null);
 }
 
 function generate_outbound(node) {
@@ -189,12 +194,12 @@ function generate_outbound(node) {
 			const output = executeCommand(`/sbin/uci -q show ${shellQuote(uciconfig)} | /bin/grep "\.grouphash='*${shellQuote(grouphash)}'*" | /usr/bin/cut -f2 -d'.'`) || {};
 			if (!isEmpty(trim(output.stdout)))
 				for (let order in split(trim(output.stdout), /\n/))
-					push(outbounds, get_tag(order) || 'cfg-' + order + '-out');
+					push(outbounds, get_tag(order, 'cfg-' + order + '-out', { "filter_nodes": node.filter_nodes, "filter_keywords": node.filter_keywords }));
 			if (!(grouphash in groups_tobe_checkedout))
 				push(groups_tobe_checkedout, grouphash);
 		}
 		for (let order in node.order) {
-			push(outbounds, get_tag(order) || 'cfg-' + order + '-out');
+			push(outbounds, get_tag(order, 'cfg-' + order + '-out', { "filter_nodes": node.filter_nodes, "filter_keywords": node.filter_keywords }));
 			if (!(order in ['direct-out', 'block-out']) && !(order in nodes_tobe_checkedout))
 				push(nodes_tobe_checkedout, order);
 		}
@@ -202,10 +207,10 @@ function generate_outbound(node) {
 			push(outbounds, 'direct-out', 'block-out');
 		return {
 			type: node.type,
-			tag: get_tag(node) || 'cfg-' + node['.name'] + '-out',
+			tag: get_tag(node, 'cfg-' + node['.name'] + '-out'),
 			/* Selector */
 			outbounds: outbounds,
-			default: node.default_selected ? (get_tag(node.default_selected) || 'cfg-' + node.default_selected + '-out') : null,
+			default: node.default_selected ? (get_tag(node.default_selected, 'cfg-' + node.default_selected + '-out')) : null,
 			/* URLTest */
 			url: node.test_url,
 			interval: node.interval,
@@ -216,7 +221,7 @@ function generate_outbound(node) {
 
 	const outbound = {
 		type: node.type,
-		tag: get_tag(node) || 'cfg-' + node['.name'] + '-out',
+		tag: get_tag(node, 'cfg-' + node['.name'] + '-out'),
 		routing_mark: strToInt(self_mark),
 
 		server: node.address,
@@ -362,7 +367,7 @@ function get_outbound(cfg) {
 			if (isEmpty(node))
 				die(sprintf("%s's node is missing, please check your configuration.", cfg));
 			else
-				return get_tag(node) || 'cfg-' + node + '-out';
+				return get_tag(node, 'cfg-' + node + '-out');
 		}
 	}
 }

root/etc/homeproxy/scripts/homeproxy.uc
@@ -176,6 +176,22 @@ export function validation(datatype, data) {
 	const ret = system(`/sbin/validate_data ${shellQuote(datatype)} ${shellQuote(data)} 2>/dev/null`);
 	return (ret === 0);
 };
+
+export function filterCheck(name, filter_mode, filter_keywords) {
+	if (isEmpty(name) || isEmpty(filter_mode) || isEmpty(filter_keywords))
+		return false;
+
+	let ret = false;
+	for (let i in filter_keywords) {
+		const patten = regexp(i);
+		if (match(name, patten))
+			ret = true;
+	}
+	if (filter_mode === 'whitelist')
+		ret = !ret;
+
+	return ret;
+};
 /* String helper end */
 
 /* String parser start */

commit aa4a48a1a7efa3efb6f1199b215633395283e2a6

fix(node): fix the issue that `subscription.filter_nodes` is still valid after being disabled
Fixes: 14c46a100a27 feat(generator/node): Add nodes filter support to `selector` node

htdocs/luci-static/resources/view/homeproxy/node.js
@@ -1465,16 +1465,15 @@ return view.extend({
 
 		o = s.taboption('subscription', form.ListValue, 'filter_nodes', _('Filter nodes'),
 			_('Drop/keep specific nodes from subscriptions.'));
-		o.value('disabled', _('Disable'));
+		o.value('', _('Disable'));
 		o.value('blacklist', _('Blacklist mode'));
 		o.value('whitelist', _('Whitelist mode'));
-		o.default = 'disabled';
-		o.rmempty = false;
+		o.default = '';
 
 		o = s.taboption('subscription', form.DynamicList, 'filter_keywords', _('Filter keywords'),
 			_('Drop/keep nodes that contain the specific keywords. <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions">Regex</a> is supported.'));
-		o.depends({'filter_nodes': 'disabled', '!reverse': true});
-		o.rmempty = false;
+		o.depends({'filter_nodes': '', '!reverse': true});
+		o.retain = true;
 
 		o = s.taboption('subscription', form.Flag, 'allow_insecure', _('Allow insecure'),
 			_('Allow insecure connection by default when add nodes from subscriptions.') +

root/etc/config/homeproxy
@@ -54,7 +54,7 @@ config homeproxy 'subscription'
 	option allow_insecure '0'
 	option packet_encoding 'xudp'
 	option update_via_proxy '0'
-	option filter_nodes 'disabled'
+	option filter_nodes ''
 
 config homeproxy 'server'
 	option enabled '0'

root/etc/homeproxy/scripts/update_subscriptions.uc
@@ -31,7 +31,7 @@ const ucimain = 'config',
       ucisubscription = 'subscription';
 
 const allow_insecure = uci.get(uciconfig, ucisubscription, 'allow_insecure') || '0',
-      filter_mode = uci.get(uciconfig, ucisubscription, 'filter_nodes') || 'disabled',
+      filter_mode = uci.get(uciconfig, ucisubscription, 'filter_nodes') || 'nil',
       filter_keywords = uci.get(uciconfig, ucisubscription, 'filter_keywords') || [],
       packet_encoding = uci.get(uciconfig, ucisubscription, 'packet_encoding') || 'xudp',
       subscription_urls = uci.get(uciconfig, ucisubscription, 'subscription_url') || [],