muink/luci-app-homeproxy

commit e6cdda2bc0c2bb8aea50a6006a9b10b718583837

feat(server/client): service quick reload

htdocs/luci-static/resources/view/homeproxy/client.js
@@ -6,6 +6,7 @@
 
 'use strict';
 'require form';
+'require fs';
 'require network';
 'require poll';
 'require rpc';
@@ -400,6 +401,13 @@ return view.extend({
 		}
 		so.default = 'nil';
 		so.rmempty = false;
+
+		so = ss.option(form.Button, '_reload_client', _('Quick Reload'));
+		so.inputtitle = _('Reload');
+		so.inputstyle = 'apply';
+		so.onclick = function() {
+			return fs.exec('/etc/init.d/homeproxy', ['reload']);
+		};
 		/* Routing settings end */
 
 		/* Routing nodes start */

htdocs/luci-static/resources/view/homeproxy/server.js
@@ -6,6 +6,7 @@
 
 'use strict';
 'require form';
+'require fs';
 'require poll';
 'require rpc';
 'require uci';
@@ -71,6 +72,13 @@ return view.extend({
 		}
 
 		s = m.section(form.NamedSection, 'server', 'homeproxy', _('Global settings'));
+
+		o = s.option(form.Button, '_reload_server', _('Quick Reload'));
+		o.inputtitle = _('Reload');
+		o.inputstyle = 'apply';
+		o.onclick = function() {
+			return fs.exec('/etc/init.d/homeproxy', ['reload']);
+		};
 
 		o = s.option(form.Flag, 'enabled', _('Enable'));
 		o.default = o.disabled;

commit 00d39d867650353902a6394093693496f37ff18d

fix(server/client): no direct external calls
Fixes: e6cdda2bc0c2 feat(server/client): service quick reload

htdocs/luci-static/resources/view/homeproxy/client.js
@@ -6,11 +6,11 @@
 
 'use strict';
 'require form';
-'require fs';
 'require network';
 'require poll';
 'require rpc';
 'require uci';
+'require ui';
 'require validation';
 'require view';
 
@@ -23,6 +23,12 @@ var callServiceList = rpc.declare({
 	method: 'list',
 	params: ['name'],
 	expect: { '': {} }
+});
+
+var callRcInit = rpc.declare({
+	object: 'rc',
+	method: 'init',
+	params: ['name', 'action']
 });
 
 var callReadDomainList = rpc.declare({
@@ -92,6 +98,17 @@ function renderStatus(isRunning, args) {
 		renderHTML = spanTemp.format('red', _('HomeProxy'), _('NOT RUNNING'));
 
 	return renderHTML;
+}
+
+function handleAction(action, ev) {
+	return callRcInit("homeproxy", action).then((ret) => {
+		if (ret)
+			throw _('Command failed');
+
+		return true;
+	}).catch((e) => {
+		ui.addNotification(null, E('p', _('Failed to execute "/etc/init.d/%s %s" action: %s').format("homeproxy", action, e)));
+	});
 }
 
 function validatePortRange(section_id, value) {
@@ -406,7 +423,7 @@ return view.extend({
 		so.inputtitle = _('Reload');
 		so.inputstyle = 'apply';
 		so.onclick = function() {
-			return fs.exec('/etc/init.d/homeproxy', ['reload']);
+			return handleAction('reload');
 		};
 		/* Routing settings end */
 

htdocs/luci-static/resources/view/homeproxy/server.js
@@ -6,7 +6,6 @@
 
 'use strict';
 'require form';
-'require fs';
 'require poll';
 'require rpc';
 'require uci';
@@ -72,13 +71,6 @@ return view.extend({
 		}
 
 		s = m.section(form.NamedSection, 'server', 'homeproxy', _('Global settings'));
-
-		o = s.option(form.Button, '_reload_server', _('Quick Reload'));
-		o.inputtitle = _('Reload');
-		o.inputstyle = 'apply';
-		o.onclick = function() {
-			return fs.exec('/etc/init.d/homeproxy', ['reload']);
-		};
 
 		o = s.option(form.Flag, 'enabled', _('Enable'));
 		o.default = o.disabled;